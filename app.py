d1a27e274a3d2c6dc46a62e0d692f31b09c3c932
Saket Kumar <saketkranshu@gmail.com>
2026-04-24 19:39:12 +0530

    added visual chart for better financial understanding

--- app.py
+++ app.py
@@ -16,12 +16,19 @@ if ticker:
     st.write("**Website:**", info.get('website'))
     st.write("**Employees:**", info.get('fullTimeEmployees'))
     st.write("**Description:**", info.get('longBusinessSummary'))
+
     st.subheader("Key Financials (in USD Billions)")
-    
+
     income_statement = company.financials
-    
     key_metrics = income_statement.loc[['Total Revenue', 'Gross Profit', 'Operating Income', 'Net Income', 'EBITDA']]
     key_metrics_clean = key_metrics.iloc[:, :4] / 1e9
     key_metrics_clean = key_metrics_clean.round(2)
-    
+
     st.dataframe(key_metrics_clean)
+
+    st.subheader("Revenue & Net Income Trend")
+
+    chart_data = key_metrics_clean.T
+    chart_data.index = [str(col)[:4] for col in chart_data.index]
+
+    st.line_chart(chart_data[['Total Revenue', 'Net Income']])
